7070e390fed868431d3b2e2629b1175fab96172f
Ubuntu <ubuntu@ip-172-31-59-29.ec2.internal>
2023-10-18 18:18:33 +0000

    Descarga de Adminer y GoAcces

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -6,7 +6,7 @@ set -x
 #-----------------------------------------------------
 #Configuramos la variables :
 
-PHPMYADMIN_APP_PASSWORD=1003
+PHPMYADMIN_APP_PASSWORD=usuario
 APP_USER=usuario
 APP_PASSWORD=contraseña
 #-----------------------------------------------------
@@ -15,7 +15,7 @@ APP_PASSWORD=contraseña
 sudo update -y
 
 #Actualizamos los paquetes:
-#sudo upgrade -y
+sudo upgrade -y
 
 #Configuramos las respuestas de las preguntas durante la instalación de Phpmyadmin:
 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
@@ -32,3 +32,81 @@ sudo apt install phpmyadmin php-mbstring php-zip php-gd php-json php-curl -y
 mysql -u root <<< "DROP USER IF EXITS 'APP_USER'@'%'"
 mysql -u root <<< "CREATE USER' $APP_USER''@''%' IDENTIFIED BY ' $APP_PASSWORD'"
 mysql -u root <<< "GRANT ALL PRIVILEGES ON *.* TO '$APP_USER'@'%'"
+
+
+#Instalamos phpmyAdmin
+apt install phpmyadmin php-mbstring php-zip php-json php-curl -y
+
+#Instalacion de Adminer(aplicacion gestions de base de datos online, github en este caso)
+
+wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql.php
+
+#Creamos directorio para Adminer
+mkdir -p /var/www/html/adminer
+
+#Movemos y renombramos el archivo
+
+mv adminer-4.8.1-mysql.php /var/www/html/adminer/index.php
+
+#-----------------------------------------------------------------------------
+#Instalamos GoAccess
+#-----------------------------------------------------------------------------
+
+echo "deb http://deb.goaccess.io/ $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/goaccess.list
+
+#Añadimos clave publiaca en la maquina
+
+wget -O - https://deb.goaccess.io/gnugpg.key | sudo apt-key add -
+
+# Actualizamos los repositorios
+
+apt update
+
+#Instalamos GoAccess
+apt install goaccess -y
+
+#Creamos el directorio stats
+
+mkdir -p /var/www/html/stats
+
+#Modificamos permisos
+chown www-data:www-data /var/www/html -R 
+
+#Ejecutamos Goacces en segundo plano
+
+goaccess /var/log/apache2/access.log -o /var/www/html/stats/index.html --log-format=COMBINED --real-time-html --daemonize
+
+#Control de acceso a un directorio con .htaccess
+#-----------------------------------------------------------
+#Creamos un directorio para guardar el archivo de claves
+mkdir -p /etc/apache2/claves
+#Creamos un usuario y contraseña en un archivo .htacces
+
+sudo htpasswd -bc /etc/apache2/claves/.htpasswd $STATS_USER $STATS_PASSWORD
+
+#Usuario 1
+
+#sudo htpasswd -c /home/usuario/.htpasswd andres andres
+
+#Copiamos el archivo de configuración de Apache
+
+#cp ../conf/000-default.conf /etc/apache2/sites-available
+
+#Reiniciamos el servicio de Apache
+
+systemctl restart apache2
+
+
+#Control de acceso a un directorio mediante .htaccess
+ 
+#------------------------------------------
+#Copiamos el archivo htacces en /var/www/html/stats
+cp ../htaccess/htaccess /var/html/stats/.htaccess
+#Copiamos el archivo de la configuración de Apache
+cp ../conf/000-default-htaccess.conf /etc/apache2/sites-avariable/000-default.conf
+
+#Reiniciamos el servicio de Apache
+
+
+sudo systemctl restart apache2.service
+sudo systemctl start apache2.service
